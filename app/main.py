2de0c262ed2adb2675835b63d22c88f865132fea
Dmorales13110 <dmorales@insightnexus.io>
2025-11-19 17:46:34 -0400

    fix: acceso de cors

--- app/main.py
+++ app/main.py
@@ -42,7 +42,7 @@ app = FastAPI(
 # CORS (se mantiene igual)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Ajusta según tus necesidades
+    allow_origins=["*"],  # Ajusta según tus necesidades
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["Authorization", "Content-Type", "Set-Cookie"],
