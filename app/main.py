7630b39fad5d508441af693170db818eaca77c5a
Dmorales13110 <dmorales@insightnexus.io>
2025-11-19 17:53:50 -0400

    fix: er credencial

--- app/main.py
+++ app/main.py
@@ -43,7 +43,6 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Ajusta según tus necesidades
-    allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["Authorization", "Content-Type", "Set-Cookie"],
 )
